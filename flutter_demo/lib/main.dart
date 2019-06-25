129d8ae97337ac01119213fcba90e478b2be692c
MatheusMassula <matheusmassula1@gmail.com>
2019-06-25 20:19:18 -0300

    Build the chat screen.

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new MaterialApp(
-      title: "Friendlychat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Friendlychat"),
-        ),
-      ),
-    ),
-  );
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Friendlychat',
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar( title: new Text('Friendlychat'),);
+  }
 }
